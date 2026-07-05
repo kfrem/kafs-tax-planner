36818352755f4f00c7449f785bd38c0c183b5da8
kfrem <kfrem@hotmail.com>
2026-07-05 12:37:54 +0100

    auto: save 2026-07-05 12:37

--- firms/urls.py
+++ firms/urls.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
+from . import mfa
+
 app_name = "firms"
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="firms/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("mfa/setup/", mfa.mfa_setup, name="mfa-setup"),
+    path("mfa/verify/", mfa.mfa_verify, name="mfa-verify"),
 ]
