e2b5fb96108138a8e2ece61ec83c5704712ea724
kfrem <kfrem@hotmail.com>
2026-07-05 12:25:21 +0100

    auto: save 2026-07-05 12:25

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -7,4 +7,6 @@ app_name = "monitoring"
 urlpatterns = [
     path("", views.queue, name="queue"),
     path("alert/<int:pk>/action/", views.alert_action, name="alert-action"),
+    path("authorities/", views.authorities, name="authorities"),
+    path("authorities/<int:pk>/status/", views.authority_status, name="authority-status"),
 ]
