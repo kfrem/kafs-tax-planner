07894cb9b87c9573aa27c3892b2807be870967d2
kfrem <kfrem@hotmail.com>
2026-07-08 08:53:22 +0100

    auto: save 2026-07-08 08:53

--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,9 @@
+import csv
+
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .csv_import import import_client_csv
