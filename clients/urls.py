33bfa3f0f343df6f12bc411ee364faaea13c808c
kfrem <kfrem@hotmail.com>
2026-07-08 08:54:01 +0100

    auto: save 2026-07-08 08:54

--- clients/urls.py
+++ clients/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", views.client_list, name="client-list"),
     path("new/", views.client_create, name="client-create"),
     path("import/", views.csv_import_view, name="csv-import"),
+    path("import/template/", views.csv_template_download, name="csv-template"),
     path("<int:pk>/", views.client_detail, name="client-detail"),
     path("<int:pk>/facts/new/", views.client_facts_create, name="client-facts-create"),
     path("<int:pk>/access/", views.client_access, name="client-access"),
